98456ff2ad5de5843f016cc76334110b13ada460
lichang <dotb116393@163.com>
2024-07-04 18:08:35 +0800

    :memo:add comments to deepClone

--- packages/utils/src/object/deepClone.ts
+++ packages/utils/src/object/deepClone.ts
@@ -7,7 +7,9 @@
  * @email dotb116393@163.com
  */
 export const deepClone = <T>(oldObj: T): T => {
+  //handle !object and null
   if (typeof oldObj !== 'object' || oldObj === null) return oldObj
+  //handle Date
   if (oldObj instanceof Date) return new Date(oldObj.getTime()) as T
   return JSON.parse(JSON.stringify(oldObj))
 }
