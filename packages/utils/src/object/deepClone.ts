4670bc3c4bcbe7232610411c7062b0fb0b06e1c4
lichang <dotb116393@163.com>
2024-07-05 11:41:13 +0800

    :hammer:refactor func deepClone -> add handler for type of Map|Set

--- packages/utils/src/object/deepClone.ts
+++ packages/utils/src/object/deepClone.ts
@@ -8,8 +8,30 @@
  */
 export const deepClone = <T>(oldObj: T): T => {
   //handle !object and null
-  if (typeof oldObj !== 'object' || oldObj === null) return oldObj
+  if (
+    typeof oldObj !== 'object' ||
+    oldObj === null ||
+    oldObj instanceof WeakMap ||
+    oldObj instanceof WeakSet
+  )
+    return oldObj
   //handle Date
   if (oldObj instanceof Date) return new Date(oldObj.getTime()) as T
+  //handle Map
+  if (oldObj instanceof Map) {
+    let newMap = new Map()
+    oldObj.forEach((value, key) => {
+      newMap.set(key, deepClone(value))
+    })
+    return newMap as T
+  }
+  //handle Set
+  if (oldObj instanceof Set) {
+    let newSet = new Set()
+    oldObj.forEach((value) => {
+      newSet.add(deepClone(value))
+    })
+    return newSet as T
+  }
   return JSON.parse(JSON.stringify(oldObj))
 }
