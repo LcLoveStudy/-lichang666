3218822490fb2d719b56c49aea2ff13539bad403
lichang <dotb116393@163.com>
2024-07-05 15:29:26 +0800

    :hammer:refactor for dayOfYear and isDateLike

--- packages/utils/src/date/dayOfYear.ts
+++ packages/utils/src/date/dayOfYear.ts
@@ -1,11 +1,24 @@
+import { isDate } from './isDate'
+import { isDateLike } from './isDateLike'
+
 /**
  * 获取某个日期位于当年的第几天
  * @param {Date} date 日期对象
  * @example
  * dayOfYear(new Date('2023-2-1')) --> 32
  */
-export const dayOfYear = (date: Date) => {
-  return Math.floor(
-    (date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) / 1000 / 60 / 60 / 24
-  )
+export const dayOfYear = (date: Date | string | number) => {
+  if (isDate(date)) {
+    date = date as Date
+    return Math.floor(
+      (date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) / 1000 / 60 / 60 / 24
+    )
+  } else if (isDateLike(date)) {
+    let d = new Date(date as string | number)
+    return Math.floor(
+      (d.getTime() - new Date(d.getFullYear(), 0, 0).getTime()) / 1000 / 60 / 60 / 24
+    )
+  } else {
+    throw new Error('dayOfYear: date is not a valid Date object')
+  }
 }
--- packages/utils/src/date/isDateLike.ts
+++ packages/utils/src/date/isDateLike.ts
@@ -1,9 +1,10 @@
+import { isDate } from './isDate'
 /** 判断参数是否是Date或者是否能被转换为Date类型
  * @param {unknown} param 参数
  * @returns {boolean} 是否是Date类型
  */
 export const isDateLike = (param: unknown): boolean => {
-  if (param instanceof Date) return true
+  if (isDate(param)) return true
   if (typeof param !== 'string' && typeof param !== 'number') return false
   if (param === '') return false
   const date = new Date(param as string | number)
