3dc4fc7e14fa934721b26bba870b3968e002dddb
lichang <dotb116393@163.com>
2024-07-08 18:03:25 +0800

    :tada:components init

--- playground/playground-vue/src/App.vue
+++ playground/playground-vue/src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <lc-card></lc-card>
+  <div class="playground">
+    <lc-card class="my-card" shadow="hover">1</lc-card>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { LcCard } from '@lichang666/lc-design-vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.playground {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #eee;
+}
+.my-card {
+  width: 20rem;
+  height: 10rem;
+}
+</style>
