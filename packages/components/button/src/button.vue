e7d905ddebec664957b24800fc2c0d9fc7803f5c
lichang <dotb116393@163.com>
2024-07-25 17:29:51 +0800

    :hammer:refactor button style

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -11,14 +11,13 @@
     :disabled="disabled || loading"
   >
     <div class="lc-button-content">
-      <loading-icon class="lc-button-loading" :color="loadingIconColor" v-show="loading" />
+      <loading-icon class="lc-button-loading" v-show="loading" />
       <slot></slot>
     </div>
   </button>
 </template>
 
 <script setup lang="ts">
-  import { computed } from 'vue'
   import LoadingIcon from '../../icons/loading.vue'
   import { buttonProps } from './button'
   import { customColorComputed } from './custom-color-computed'
@@ -26,15 +25,15 @@
   const { hoverBgColor, activeBgColor, disabledBgColor, plainBgColor } = customColorComputed(
     props.color as string
   )
-  const loadingIconColor = computed(() => {
-    return props.type === 'default' || props.plain ? '#000' : '#fff'
-  })
 </script>
 
 <style lang="less" scoped>
   .lc-button-custom {
     background: v-bind(color);
     border-color: v-bind(color);
+    .lc-button-loading {
+      fill: #fff;
+    }
     &:hover {
       background-color: v-bind(hoverBgColor);
     }
@@ -44,6 +43,7 @@
     &::after {
       box-shadow: 0 0 10px v-bind(activeBgColor);
     }
+
     &:disabled {
       border-color: v-bind(disabledBgColor);
       background-color: v-bind(disabledBgColor);
@@ -52,6 +52,9 @@
     &.lc-button-plain {
       background-color: v-bind(plainBgColor);
       color: v-bind(color);
+      .lc-button-loading {
+        fill: v-bind(color);
+      }
       &:hover {
         background-color: v-bind(color);
         color: #fff;
