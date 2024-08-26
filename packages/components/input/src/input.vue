d62226b04ef1a34b1bc0a49460103e03be2c4f1f
lichang <dotb116393@163.com>
2024-08-26 14:24:09 +0800

    :new:add input emits

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="lc-input">
-    <input class="lc-input__inner" v-model="inputValue" :placeholder />
+    <input
+      class="lc-input__inner"
+      v-model="inputValue"
+      :placeholder
+      @input="inputEventHandler"
+      @blur="blurEventHandler"
+      @focus="focusEventHandler"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
   import { inputProps } from './input'
   defineProps(inputProps)
+  const emits = defineEmits(['input', 'blur', 'focus'])
   const inputValue = defineModel({
     type: String,
     required: true
   })
+
+  /** input event */
+  const inputEventHandler = () => {
+    emits('input', inputValue.value)
+  }
+
+  /** blur event */
+  const blurEventHandler = (e: FocusEvent) => {
+    emits('blur', e)
+  }
+
+  /** focus event */
+  const focusEventHandler = (e: FocusEvent) => {
+    emits('focus', e)
+  }
 </script>
